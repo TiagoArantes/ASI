c84830369292a8896b271bb57e3fc59b6842cde7
TiagoArantes <tiagoufu72@hotmail.com>
2018-06-17 22:36:14 -0300

    com autenticacao

---
+++ DrogariaSI/src/main/java/br/com/DrogariaSI/bean/FabricanteBean.java
@@ -0,0 +1,96 @@
+package br.com.DrogariaSI.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+
+import org.omnifaces.util.Messages;
+
+import br.com.DrogariaSI.dao.FabricanteDAO;
+import br.com.DrogariaSI.domain.Fabricante;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class FabricanteBean implements Serializable {
+	private Fabricante fabricante;
+	private List<Fabricante> fabricantes;
+
+	public Fabricante getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(Fabricante fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public List<Fabricante> getFabricantes() {
+		return fabricantes;
+	}
+
+	public void setFabricantes(List<Fabricante> fabricantes) {
+		this.fabricantes = fabricantes;
+	}
+
+	@PostConstruct
+	public void listar() {
+		try {
+			FabricanteDAO fabricanteDAO = new FabricanteDAO();
+			fabricantes = fabricanteDAO.listar();
+ 		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar os fabricantes");
+			erro.printStackTrace();
+		}
+	}
+
+	public void novo() {
+		fabricante = new Fabricante();
+	}
+
+	public void salvar() {
+		try {
+			FabricanteDAO fabricanteDAO = new FabricanteDAO();
+			fabricanteDAO.merge(fabricante);
+
+			novo();
+			fabricantes = fabricanteDAO.listar();
+
+			Messages.addGlobalInfo("Fabricante salvo com sucesso");
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar salvar o fabricante");
+			erro.printStackTrace();
+		}
+	}
+
+	public void excluir(ActionEvent evento) {
+		try {
+			fabricante = (Fabricante) evento.getComponent().getAttributes().get("fabricanteSelecionado");
+
+			FabricanteDAO fabricanteDAO = new FabricanteDAO();
+			fabricanteDAO.excluir(fabricante);
+
+			fabricantes = fabricanteDAO.listar();
+
+			Messages.addGlobalInfo("Fabricante removido com sucesso");
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar remover o fabricante");
+			erro.printStackTrace();
+		}
+	}
+
+	public void editar(ActionEvent evento) {
+		fabricante = (Fabricante) evento.getComponent().getAttributes().get("fabricanteSelecionado");
+		try {
+			fabricante = (Fabricante) evento.getComponent().getAttributes().get("fabricanteSelecionado");
+
+			Messages.addGlobalInfo("Fabricante editado com sucesso");
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar editar o fabricante");
+			erro.printStackTrace();
+		}
+	}
+}
